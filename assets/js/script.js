b10e14659e953429eb482f4db8ccc50a30f87458
Zach Fulmer <Zachfulmer@earthlink.net>
2022-05-16 17:28:36 -0500

    dynamic form complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,24 @@ var createTaskHandler = function(event)
 {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create ;ost ote,
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task!";
+
+    // create dov to hold task info and add to list item 
+    var taskInfoEl = document.createElement("div");
+
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    
+    // add html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>"+taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 };
 
